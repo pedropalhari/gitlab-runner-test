5e9a7d65341cd5192d620edb8632de3c03386a3b
Pedro Palhari <pedropalhari@gmail.com>
2020-09-17 13:35:34 -0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React2
+          Learn React23
         </a>
       </header>
     </div>
